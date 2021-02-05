4fd84a1fbc8bc91b80e7c87838292b72c1906262
Michał Kupczak <kurczak12333@gmail.com>
2021-02-05 17:53:03 +0100

    Add type conversions

---
+++ JavaScript/TypeConversions.js
@@ -0,0 +1,8 @@
+if("") false //Nie wykona się
+if("t") true //wykona się
+
+Math.floor(-5.4) //-6! pierwsza mniejsza
+Math.ceil(-5.5) //-5 pierwsza wyższa
+
+var a = Number("55a"); //55
+var b = String(5); //"5"

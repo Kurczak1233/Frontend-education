728eac0bb39ccdab3009ea121e072fbb5b145b2a
Michał Kupczak <kurczak12333@gmail.com>
2021-02-05 17:18:13 +0100

    Further leraning process - operators

--- JavaScript/Operators.js
+++ JavaScript/Operators.js
@@ -19,3 +19,24 @@ console.log("Po wywołaniu " + a) //2
 // === Operator ścisłsłego porównania. Sprawdza wartości i typy
 if(5 == "5") true //Bo zostanie skonwertowany string do num
 if(5 === "5") false //Bo nie będzie konwersji.
+
+//Porównania obiektów
+// === dla obiektów sprawdza REFERENCJE
+let c = {data: 22};
+let d = {data: 22};
+if(c === d) false //Nie wykona się bo referencje są różne!
+
+//Operatory relacyjne
+let num = {num: 10};
+if("num" in obj) console.log("zmienna num w obj"); //in sprawdza czy coś jest w obiekcie
+
+let arr = ["one", "two", "three"]
+if(1 in arr) console.log("udało się");
+
+var type = typeof arr //Array
+
+var data = new Date(Getdate());
+if(date instanceof Date) true;
+
+//Operator trójargumentówy
+var data = (5 > 8) ? 5 : 8; //True = 5  ,false = 8

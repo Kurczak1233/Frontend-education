25e4a5e8a89f1108a043194dea13e1470fdd37cc
Michał Kupczak <kurczak12333@gmail.com>
2021-02-05 16:11:12 +0100

    Operatory porónania

--- JavaScript/Operators.js
+++ JavaScript/Operators.js
@@ -12,3 +12,10 @@ console.log("Po wywołaniu " + a) //2
 console.log("Przed wywołaniem: " + a); //2
 printVar(++a); //PREINKREMENTACJA! (pierwsze zinkrementuje, potem funkcja) //Wartość w funkcji : 2
 console.log("Po wywołaniu " + a) //2
+
+//Ścisłe operatory porównania
+// == vs ===
+// == Operator luźnego porównania. Sprawdza wartości
+// === Operator ścisłsłego porównania. Sprawdza wartości i typy
+if(5 == "5") true //Bo zostanie skonwertowany string do num
+if(5 === "5") false //Bo nie będzie konwersji.

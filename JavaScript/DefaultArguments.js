edbee8d45b9e9041df436e2110e62f5ad1c3c732
Michał Kupczak <kurczak12333@gmail.com>
2021-02-07 14:41:57 +0100

    defaultarguments

---
+++ JavaScript/DefaultArguments.js
@@ -0,0 +1,14 @@
+function makeConnection(host = "google.com", port=80, method="HTTP")
+
+makeConnection() // Zostaną przydzielonme argumenty domyślne
+
+function incrementation( arg=increment() )
+{
+    console.log(arg)
+}
+let a = 0
+function increment()
+{
+    return ++a;
+}
+incrementation();

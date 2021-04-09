bab35ba665908d5398df30fa0452a2519cee709c
Kurczak1233 <kurczak12333@gmail.com>
2021-04-09 20:32:14 +0200

    Classes + inheritance

--- React/educationpurpose/src/Js Instances/Classes.js
+++ React/educationpurpose/src/Js Instances/Classes.js
@@ -9,12 +9,39 @@ export function Test()
             this.name = name;
             this.capital = capital;
             this.population = population;
+            this.showName = () => console.log(this.name);
+        }
+        showCountryName()
+        {
+            console.log(`Nazwa kraju to ${this.name}`)
         }
     }
     const poland = new Country('Polska', 'Warsaw', 38800000);
     
+    //Dziedziczenie
+
+    class Person {
+        constructor(name)
+        {
+            this.name = name;
+        }
+        showName() {
+            console.log(`Imie tego osobnika to ${this.name}`)
+        }
+    }
+
+    class Student extends Person {
+        constructor(name = "", degrees = [])
+        {
+            super(name);
+            this.degrees = degrees;
+        }
+        showDegrees(){
+            const completed = this.degrees.filter(degree => degree > 2)
+            console.log(`Studenciak ${this.Name} ma stopnie ${this.degrees} i zaliczył już ${(this.completed.length)}`)
+        }
+    }
 
 
 
-    
 }

be0440b648865176f76b82d88386c6d1c5b4849b
Kurczak1233 <kurczak12333@gmail.com>
2021-04-10 22:02:22 +0200

    Hide/Show button

---
+++ React/educationpurpose/src/React practical/React Hide&Show.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Message extends React.Component{
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            isButtonActive: false 
+        }
+    }
+    handlerClick= () => {
+        
+        this.setState({
+            isActive: !this.state.isButtonActive
+        })
+    }
+    render()
+    {
+        const text = "lorem"
+        return(
+        
+            <React.Fragment>
+                <button onClick={this.handlerClick}>{this.state.isButtonActive ? "Ukryj":"Pokaż"}</button>
+                {this.state.isButtonActive ? <p>{text}</p> : null}
+            </React.Fragment>
+        )
+    }
+}
+
+ReactDOM.render(<Message />, document.getElementById("root"))

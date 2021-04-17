06e25e55368a73ff05f3b303d6d5f42cbdb83fe6
Kurczak1233 <kurczak12333@gmail.com>
2021-04-17 21:45:53 +0200

    setInterval function

--- React-create-app education/advancededucation/src/App.js
+++ React-create-app education/advancededucation/src/App.js
@@ -2,41 +2,50 @@ import React from 'react'
 import './App.css';
 import SwitchButton from './SwitchButton'
 
-class App extends React.Component {
-  constructor(props)
-  {
-    super(props)
-    this.state = {
-      time: 0,
-      isActive: false,
-    }
-  }
-  handleStartTime = () => {
-    this.setState({
-      isActive: !this.state.isActive
+const App = () => {
+  // state = {
+  //   time: 0,
+  //   isActive: false,
+  // }
+  const data = [
+    { id: 1, title: 'Wiadomość 1', body:'Zawartość...'},
+    { id: 2, title: 'Wiadomość 2', body:'Zawartość...'}
+  ]
+  setInterval(() => {
+    const index = data.length +1;
+    data.push({
+      id: index,
+      title: `Wiadomość + ${index}`,
+      body: "Zawartość", 
     })
-    if(this.state.isActive === true)
-    {
-      clearInterval(this.IdInterval)
-    }
-    else{
-      this.IdInterval = setInterval(() => this.addSecond(), 1000)
-    }
-  }
-  addSecond = () => {
-    this.setState({
-      time: this.state.time + 1
-    })
-  }
-  render()
-  { 
+    console.log(data);
+  }, 4000)
+
+  // handleStartTime = () => {
+  //   this.setState({
+  //     isActive: !this.state.isActive
+  //   })
+  //   if(this.state.isActive === true)
+  //   {
+  //     clearInterval(this.IdInterval)
+  //   }
+  //   else{
+  //     this.IdInterval = setInterval(() => this.addSecond(), 1000)
+  //   }
+  // }
+  // addSecond = () => {
+  //   this.setState({
+  //     time: this.state.time + 1
+  //   })
+  // }
     return(
       <React.Fragment>
-        <div className="text-center">{this.state.time}</div>
-        <SwitchButton click={this.handleStartTime} active={this.state.isActive}></SwitchButton>
+        {/* <div className="text-center">{this.state.time}</div>
+        <SwitchButton click={this.handleStartTime} active={this.state.isActive}></SwitchButton> */}
+        
       </React.Fragment>
     );
   }
-}
+
 
 export default App;

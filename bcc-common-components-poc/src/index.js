47e5eeb2dc4ab4eee1dd2346a7518cbd158b3936
Michal Kupczak <kurczak12333@gmail.com>
2022-11-23 12:01:32 +0100

    Added second package purely for vue components

---
+++ bcc-common-components-poc/src/index.js
@@ -0,0 +1,7 @@
+import LearnVueButton from "./LearnVueButton/LearnVueButton.vue";
+
+export default {
+  install: (app, options) => {
+    app.component("LearnVueButton", LearnVueButton);
+  },
+};
---
+++ bcc-common-components-poc/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "lv-button",
+      fileName: (format) => `${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+  },
+  plugins: [vue()],
+});

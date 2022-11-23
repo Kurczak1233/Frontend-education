05d8564b409a17270983ef6aa0f929d5cb0f1215
Michal Kupczak <kurczak12333@gmail.com>
2022-11-23 12:21:16 +0100

    Created config for tailwind and accordeon component

--- bcc-common-components-poc/src/index.js
+++ bcc-common-components-poc/src/index.js
@@ -1,7 +1,12 @@
 import LearnVueButton from "./LearnVueButton/LearnVueButton.vue";
+import Accordion from "./TailwindAccordion/Accordion.vue";
+import "./index.css";
+import "flowbite";
+import "../node_modules/flowbite/dist/flowbite.js";
 
 export default {
   install: (app, options) => {
     app.component("LearnVueButton", LearnVueButton);
+    app.component("Accordion", Accordion);
   },
 };
